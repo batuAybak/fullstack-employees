3575ad3973c7e02424cae9a8fb1d755bfbefe21b
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-08-29 16:00:13 -0400

    add first part of assignment 01-database

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,6 @@
 import db from "#db/client";
+import { faker } from '@faker-js/faker'
+import { createEmployee } from "./queries/employees.js";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +8,10 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  for (let i = 0; i < 10; i++) {
+    const name = faker.person.firstName()
+    const birthday = faker.date.between({ from: '1950-01-01', to: '2000-01-01' })
+    const salary = Math.floor((Math.random() + 1) * 100000)
+    await createEmployee({ name, birthday, salary })
+  }
 }
