04deab8bca8db60f3b47871c4e6490229eb8fc6f
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-09-02 19:06:27 -0400

    no try catch on sql

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -2,27 +2,20 @@ import db from '#db/client'
 
 /** @returns the employee created according to the provided details */
 export async function createEmployee({ name, birthday, salary }) {
-  try {
-    const SQL = `INSERT INTO employees(name, birthday, salary) VALUES($1, $2, $3) RETURNING *` //returns what's added
-    const { rows: addedEmployees } = await db.query(SQL, [name, birthday, salary]) // returns values of rows from the obj
-    // console.log(await getEmployees()) // TODO Delete it at the end, just added for debugging.
-    return addedEmployees
-  } catch (error) {
-    console.error(error);
-  }
+  const SQL = `INSERT INTO employees(name, birthday, salary) VALUES($1, $2, $3) RETURNING *` //returns what's added
+  const { rows: addedEmployees } = await db.query(SQL, [name, birthday, salary]) // returns values of rows from the obj
+  // console.log(await getEmployees()) // TODO Delete it at the end, just added for debugging.
+  return addedEmployees
+
 }
 
 // === Part 2 ===
 
 /** @returns all employees */
 export async function getEmployees() {
-  try {
-    const SQL = `SELECT * FROM employees`
-    const { rows: employees } = await db.query(SQL) // returns values of rows from the obj
-    return employees
-  } catch (error) {
-    console.error(error)
-  }
+  const SQL = `SELECT * FROM employees`
+  const { rows: employees } = await db.query(SQL) // returns values of rows from the obj
+  return employees
 }
 
 /**
